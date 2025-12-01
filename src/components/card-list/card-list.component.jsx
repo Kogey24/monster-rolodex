59d4d03579daf7aa3d04700fc28d832891aa770f
Harmmerton Kogi <hammertonkogey@gmail.com>
2025-12-02 00:36:02 +0300

    Adding files to github pages

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './card-list.style.css'
+import '../card/card.component'
+import { Card } from '../card/card.component';
+
+
+export const CardList = props => (
+    <div className='card-list'>
+        {
+          props.monsters.map((monster) => {
+            return <Card key={monster.id} monster={monster} />
+          })
+        }
+     
+    </div>
+);
